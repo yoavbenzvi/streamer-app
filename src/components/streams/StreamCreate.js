9ef6d349f38af5ce166422843d01cfdbe9de8395
Yoav <yoav.benzvi@gmail.com>
2020-04-23 19:44:22 +0200

    started work on stream form
    -installed redux-form package
    -craeted controlled input fields using redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-	return(
-		<div>
-			StreamCreate
-		</div>
-	)
+class StreamCreate extends React.Component {
+
+	//1. - the hard way
+	// renderInput = (formProps) => <input 
+	// 	onChange={formProps.input.onChange} 
+	// 	value={formProps.input.value}
+	// />
+
+	//2. - the easy way
+	// renderInput = (formProps) => <input {...formProps.input}/>
+
+	//3. - the smart way (destructuring)
+	// renderInput = ({ input }) => <input {...input}/>
+
+	//4. - conditional - passing props to Field component
+	renderInput = ({ input, label }) => {
+		return(
+			<div>
+				<label>
+					{label}
+				</label>
+				<input {...input}/>
+			</div>
+		)
+	}
+
+	render() {	
+		console.log(this.props)
+		return(
+			<form className='ui form'>
+				<Field name='title' component={this.renderInput} label="Enter Title"/>
+				<Field name='description' component={this.renderInput} label="Enter Description"/>
+			</form>
+		)
+	}
 }
 
-export default StreamCreate;
+export default reduxForm({
+	form: 'streamCreate'
+})(StreamCreate);
