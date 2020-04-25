0cf7bc25fc5a17c63b2cd30b38e054f31f2a5759
Yoav <yoav.benzvi@gmail.com>
2020-04-25 13:07:46 +0200

    created a reusable Modal component
    -Modal users the Portal method by rendering on a div in index.html
    -modal is reusable and accepts props for displayed content

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+
+	const actions = (
+		<div>
+			<button className='ui center negative button'>Delete</button>
+			<button className='ui button'>Cancel</button>
+		</div>
+	);
+
 	return(
 		<div>
 			StreamDelete
+			<Modal 
+				title="Delete Stream"
+				content="Are you sure?"
+				actions={actions}
+				onDismiss={() => history.push('/')}
+			/>
 		</div>
 	)
 }
